2258eedcd42ae14121cf84ee67858d8562a58c11
Aleksei Tsikov <atsikov@evolutiongaming.com>
2018-03-08 14:06:45 +0200

    Reorganize folders to be able to use as module

---
+++ src/start.js
@@ -0,0 +1,24 @@
+const app = require("./app");
+
+const rePort = /^\d+$/;
+const reHostPort = /^([a-zA-Z0-9.-_]+)\:(\d+)$/;
+
+let host = "localhost";
+let port = 8765;
+const customHostPort = process.argv[2];
+if (customHostPort) {
+    if (customHostPort.match(rePort)) {
+        port = parseInt(customHostPort);
+    } else if (parsed = customHostPort.match(reHostPort)) {
+        host = parsed[1];
+        port = parsed[2];
+    } else {
+        console.error(chalk.red(`
+Argument Error: "${customHostPort}" couldn't be matched as <port> or <host>:<port>!
+Usage examples: "yarn start", "yarn start 1234", "yarn start hostname:1234"
+`));
+        process.exit(1);
+    }
+}
+
+const server = new app(host, port);
